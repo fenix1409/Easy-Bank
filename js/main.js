687d3aa6f94d14cea19b98de60dad3d3233c7085
fenix1409 <b2550486@gmail.com>
2024-07-19 17:42:00 +0500

    add html scss

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let elModalWrapper = document.querySelector(".modal__wrapper")
+let elCloseIcon = document.querySelector(".header__close-img")
+
+
+function handleMenuIconClick(){
+    elModalWrapper.classList.toggle("show") 
+}
